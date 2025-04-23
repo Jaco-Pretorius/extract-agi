91bf1c372bd5d85a0410c223a0a17b610724939a
Jaco Pretorius <me@jacopretorius.net>
2025-04-23 18:07:22 -0500

    More cleanups and simplifications

--- parse_words.rb
+++ parse_words.rb
@@ -7,46 +7,51 @@ module WordsParser
   BIG_ENDIAN_UNSIGNED_SIXTEEN_BIT = 'n'
   UNSIGNED_EIGHT_BIT = 'C'
 
-  def self.parse_words(file_path)
-    dictionary = {}
-    previous_word = ''
-    current_word = ''
-    File.open(file_path, 'rb') do |file|
-      file.seek(1, IO::SEEK_SET)
-      initial_position = file.readbyte
-
-      file.seek(initial_position, IO::SEEK_SET)
-      loop do
-        previous_word = current_word
-        current_word = ''
-
-        break unless (byte = file.read(1))
-
-        prefix_length = byte.unpack1(UNSIGNED_EIGHT_BIT)
-        current_word = previous_word[0, prefix_length]
-
+  class << self
+    def parse_words(file_path)
+      dictionary = {}
+      previous_word = ''
+      current_word = ''
+      File.open(file_path, 'rb') do |file|
+        file.seek(1, IO::SEEK_SET)
+        file.seek(file.readbyte, IO::SEEK_SET)
         loop do
           break unless (byte = file.read(1))
 
-          value = byte.unpack1(UNSIGNED_EIGHT_BIT)
-          if value > 127
-            current_word << ((value - 128) ^ 0x7F).chr
-            break
-          else
-            current_word << (value ^ 0x7F).chr
-          end
+          previous_word = current_word
+          prefix_length = byte.unpack1(UNSIGNED_EIGHT_BIT)
+          current_word = previous_word[0, prefix_length]
+          current_word << parse_word(file)
+
+          word_number_bytes = file.read(2)
+          break unless word_number_bytes
+
+          word_number = word_number_bytes.unpack1(BIG_ENDIAN_UNSIGNED_SIXTEEN_BIT)
+          dictionary[word_number] ||= []
+          dictionary[word_number] << current_word
         end
-
-        word_number_bytes = file.read(2)
-        break unless word_number_bytes
-
-        word_number = word_number_bytes.unpack1(BIG_ENDIAN_UNSIGNED_SIXTEEN_BIT)
-        dictionary[word_number] ||= []
-        dictionary[word_number] << current_word
       end
+
+      dictionary
     end
 
-    dictionary
+    private
+
+    def parse_word(file)
+      word = String.new
+      loop do
+        break unless (byte = file.read(1))
+
+        value = byte.unpack1(UNSIGNED_EIGHT_BIT)
+        if value > 127
+          word << ((value - 128) ^ 0x7F).chr
+          break
+        else
+          word << (value ^ 0x7F).chr
+        end
+      end
+      word
+    end
   end
 end
 
